140971a9ef57908df171353d342421e7a3294ef6
Hunter Finch <hunter.finch@icloud.com>
2023-01-28 21:40:38 -0600

    added c++ stuff

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
-    <div className="main">
-      <h1>Workspace</h1>
-      <h2>Tamuhack 2023 CBRE Challenge</h2>
-      <Link href="/boxes">Floor Visualization</Link>
+    <div>
+      <div className="main">
+        <h1>Workspace</h1>
+        <h2>Tamuhack 2023 CBRE Challenge</h2>
+      </div>
+      
+      <div class="button">
+        <h1>
+        <Link href="/boxes">Floor Visualization</Link>
+        </h1>
+      </div>
     </div>
+    
   )
 }
